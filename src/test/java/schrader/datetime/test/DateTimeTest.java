acd29abbf7a9785a45287f98fe019238b0b8a9a6
Gerrit Schrader <gerrit.schrader@infinit-services.de>
2019-04-11 15:39:41 +0200

    refactored

--- src/test/java/schrader/datetime/test/DateTimeTest.java
+++ src/test/java/schrader/datetime/test/DateTimeTest.java
@@ -10,45 +10,44 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAdjusters;
 import java.util.Date;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DateTimeTest {
 
     private final static LocalDate NOW = LocalDate.now();
-    // don't use YYYY which is "week of years"
-    private final static DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     /*
      * Manipulate LocalDateTime.
      */
+
     @Test
     public void addDayAndConvertLocalDateTime2Date() {
         LocalDateTime tomorrow = NOW.plusDays(1).atTime(12, 0, 0);
-        /* Date d = */
         Date.from(tomorrow.atZone(ZoneId.systemDefault()).toInstant()); // convert to 'Date'
     }
 
     @Test
     public void subtractHoursAndMinutes() {
         LocalDateTime today = LocalDateTime.now();
-        /* LocalDateTime later = */
         today.minusHours(5).minusMinutes(30);
     }
 
     @Test
-    public void useTemporalAdjusters() {
+    public void firstDayOfMonth() {
         LocalDate date = LocalDate.of(2018, Month.FEBRUARY, 22); // 22.02.2018
-        // first day of february - 01.02.2018
         LocalDate firstDayOfMonth = date.with(TemporalAdjusters.firstDayOfMonth());
-        assertEquals(1, firstDayOfMonth.getDayOfMonth());
-        assertTrue(Month.FEBRUARY == firstDayOfMonth.getMonth());
-        assertEquals(2018, firstDayOfMonth.getYear());
-        // last day of february - 28.02.2018
+        assertThat(firstDayOfMonth.getDayOfMonth()).isEqualTo(1);
+        assertThat(firstDayOfMonth.getMonth()).isEqualTo(Month.FEBRUARY);
+        assertThat(firstDayOfMonth.getYear()).isEqualTo(2018);
+    }
+
+    @Test
+    public void lastDayOfMonth() {
+        LocalDate date = LocalDate.of(2018, Month.FEBRUARY, 22); // 22.02.2018
         LocalDate lastDayOfMonth = date.with(TemporalAdjusters.lastDayOfMonth());
-        assertEquals(28, lastDayOfMonth.getDayOfMonth());
-        assertTrue(Month.FEBRUARY == lastDayOfMonth.getMonth());
-        assertEquals(2018, lastDayOfMonth.getYear());
+        assertThat(lastDayOfMonth.getDayOfMonth()).isEqualTo(28);
+        assertThat(lastDayOfMonth.getMonth()).isEqualTo(Month.FEBRUARY);
+        assertThat(lastDayOfMonth.getYear()).isEqualTo(2018);
     }
 
     /*
@@ -56,95 +55,93 @@ public class DateTimeTest {
      */
 
     @Test
-    public void parseDate() {
-        String dateString = "19/02/2018";
+    public void parseLocalDate() {
+        String dateString = "09/09/1961";
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate date = LocalDate.parse(dateString, formatter);
-        assertEquals(19, date.getDayOfMonth());
-        assertTrue(Month.FEBRUARY == date.getMonth());
-        assertEquals(2018, date.getYear());
+        LocalDate localDate = LocalDate.parse(dateString, formatter);
+        assertThat(localDate.getDayOfMonth()).isEqualTo(9);
+        assertThat(localDate.getMonth()).isEqualTo(Month.SEPTEMBER);
+        assertThat(localDate.getYear()).isEqualTo(1961);
     }
 
     @Test
-    public void parseTime() {
-        String timeString = "09:40:59";
+    public void parseLocalTime() {
+        String timeString = "23:59:59";
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        LocalTime time = LocalTime.parse(timeString, formatter);
-        assertEquals(9, time.getHour());
-        assertEquals(40, time.getMinute());
-        assertEquals(59, time.getSecond());
+        LocalTime localTime = LocalTime.parse(timeString, formatter);
+        assertThat(localTime.getHour()).isEqualTo(23);
+        assertThat(localTime.getMinute()).isEqualTo(59);
+        assertThat(localTime.getSecond()).isEqualTo(59);
     }
 
     @Test
-    public void parseDateTime() {
-        String dateString = "19/02/2018 09:40:59";
+    public void parseLocalDateTime() {
+        String dateString = "09/09/1961 23:59:59";
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-        LocalDateTime dateTime = LocalDateTime.parse(dateString, formatter);
-        assertEquals(19, dateTime.getDayOfMonth());
-        assertTrue(Month.FEBRUARY == dateTime.getMonth());
-        assertEquals(2018, dateTime.getYear());
-        assertEquals(9, dateTime.getHour());
-        assertEquals(40, dateTime.getMinute());
-        assertEquals(59, dateTime.getSecond());
+        LocalDateTime localDateTime = LocalDateTime.parse(dateString, formatter);
+        assertThat(localDateTime.getDayOfMonth()).isEqualTo(9);
+        assertThat(localDateTime.getMonth()).isEqualTo(Month.SEPTEMBER);
+        assertThat(localDateTime.getYear()).isEqualTo(1961);
+        assertThat(localDateTime.getHour()).isEqualTo(23);
+        assertThat(localDateTime.getMinute()).isEqualTo(59);
+        assertThat(localDateTime.getSecond()).isEqualTo(59);
     }
 
     @Test
     public void isAfter() {
         LocalDate today = LocalDate.now();
         LocalDate tomorrow = today.plusDays(1);
-        assertTrue(tomorrow.isAfter(today));
+        assertThat(tomorrow.isAfter(today)).isTrue();
     }
 
     @Test
     public void isBefore() {
         LocalDate today = LocalDate.now();
         LocalDate tomorrow = today.plusDays(1);
-        assertTrue(today.isBefore(tomorrow));
+        assertThat(today.isBefore(tomorrow)).isTrue();
     }
 
     @Test
     public void isEqual() {
         LocalDate today = LocalDate.now();
-        assertTrue(today.isEqual(today));
+        assertThat(today.isEqual(today)).isTrue();
     }
 
     @Test
-    public void firstDayOfMonth() {
+    public void getFirstDayOfMonth() {
         LocalDate date = LocalDate.of(2018, Month.FEBRUARY, 22);
         int dayOfMonth = date.with(TemporalAdjusters.firstDayOfMonth()).getDayOfMonth();
-        assertEquals(1, dayOfMonth);
+        assertThat(dayOfMonth).isEqualTo(1);
     }
 
     @Test
-    public void lastDayOfMonth() {
+    public void getLastDayOfMonth() {
         LocalDate date = LocalDate.of(2018, Month.FEBRUARY, 22);
         int dayOfMonth = date.with(TemporalAdjusters.lastDayOfMonth()).getDayOfMonth();
-        assertEquals(28, dayOfMonth);
+        assertThat(dayOfMonth).isEqualTo(28);
     }
 
     @Test
-    public void testStartAndEndOfDay() {
-        LocalDateTime startOfDay = NOW.minusDays(1).atStartOfDay();
-        LocalDateTime endOfDay = startOfDay.with(LocalTime.MAX);
-        assertEquals("2018-05-28T00:00", startOfDay.toString());
-        assertEquals("2018-05-28T23:59:59.999999999", endOfDay.toString());
+    public void atStartOfDay() {
+        LocalDate localDate = LocalDate.of(1961, Month.SEPTEMBER, 9);
+        LocalDateTime atStartOfDay = localDate.atStartOfDay();
+        assertThat(atStartOfDay.toString()).isEqualTo("1961-09-09T00:00");
     }
 
     @Test
-    public void testGetMonthOfDate() {
-        LocalDateTime dateTime = LocalDate.of(2018, Month.FEBRUARY, 22).atStartOfDay();
-        Instant instant = dateTime.atZone(ZoneId.systemDefault()).toInstant();
-        long timestamp = instant.toEpochMilli();
-        // convert epoch time to local date/time
-        Instant instant2 = Instant.ofEpochMilli(timestamp);
-        LocalDateTime dateTime2 = instant2.atZone(ZoneId.systemDefault()).toLocalDateTime();
-        // day
-        assertEquals(31 + 22, dateTime2.getDayOfYear());
-        // month
-        assertEquals(Month.FEBRUARY, dateTime2.getMonth());
-        assertEquals(1, dateTime2.getMonth().ordinal());
-        // year
-        assertEquals(2018, dateTime2.getYear());
+    public void atEndOfDay() {
+        LocalDate localDate = LocalDate.of(1961, Month.SEPTEMBER, 9);
+        LocalDateTime atStartOfDay = localDate.atStartOfDay();
+        LocalDateTime atEndOfDay = atStartOfDay.with(LocalTime.MAX);
+        assertThat(atEndOfDay.toString()).isEqualTo("1961-09-09T23:59:59.999999999");
+    }
+
+    @Test
+    public void getMonthOfDate() {
+        LocalDateTime localDateTime = LocalDate.of(1961, Month.SEPTEMBER, 9).atStartOfDay();
+        assertThat(localDateTime.getMonth()).isEqualTo(Month.SEPTEMBER);
+        assertThat(localDateTime.getMonth().ordinal()).isEqualTo(9 - 1);
+        assertThat(localDateTime.getMonthValue()).isEqualTo(9);
     }
 
     /**
@@ -155,48 +152,36 @@ public class DateTimeTest {
     public void currentYear() {
         LocalDate startDate = NOW.withDayOfYear(1);
         LocalDate endDate = NOW;
-        assertEquals("01/01/" + startDate.getYear(), startDate.format(FORMAT));
-        assertEquals(endDate.getDayOfMonth() + "/" + endDate.getMonthValue() + "/" + endDate.getYear(), endDate.format(FORMAT));
     }
 
     @Test
     public void lastYear() {
         LocalDate startDate = NOW.minusYears(1).withDayOfYear(1);
         LocalDate endDate = NOW.minusYears(1).withDayOfYear(365);
-        assertEquals("01/01/" + startDate.getYear(), startDate.format(FORMAT));
-        assertEquals("31/12/" + endDate.getYear(), endDate.format(FORMAT));
     }
 
     @Test
     public void last12Months() {
         LocalDate startDate = NOW.minusMonths(12);
         LocalDate endDate = NOW;
-        assertEquals("23/03/2017", startDate.format(FORMAT));
-        assertEquals("23/03/2018", endDate.format(FORMAT));
     }
 
     @Test
     public void last3Years() {
         LocalDate startDate = NOW.minusYears(3).withDayOfYear(1);
         LocalDate endDate = NOW.minusYears(1).withDayOfYear(365);
-        assertEquals("01/01/2015", startDate.format(FORMAT));
-        assertEquals("31/12/2017", endDate.format(FORMAT));
     }
 
     @Test
     public void currentMonth() {
         LocalDate startDate = NOW.withDayOfMonth(1);
         LocalDate endDate = NOW;
-        assertEquals("01/03/2018", startDate.format(FORMAT));
-        assertEquals("23/03/2018", endDate.format(FORMAT));
     }
 
     @Test
     public void lastMonth() {
         LocalDate startDate = NOW.minusMonths(1).withDayOfMonth(1);
         LocalDate endDate = NOW.minusMonths(1).withDayOfMonth(28);
-        assertEquals("01/02/" + startDate.getYear(), startDate.format(FORMAT));
-        assertEquals("28/02/" + endDate.getYear(), endDate.format(FORMAT));
     }
 
     @Test
@@ -204,47 +189,36 @@ public class DateTimeTest {
         LocalDate yesterday = NOW.minusDays(1);
         LocalDate startDate = yesterday.minusDays(15);
         LocalDate endDate = yesterday;
-        assertEquals("28/02/2018", startDate.format(FORMAT));
-        assertEquals("23/03/2018", endDate.format(FORMAT));
     }
 
     @Test
     public void currentAndLastYear() {
         LocalDate startDate = NOW.minusYears(1).withDayOfYear(1);
         LocalDate endDate = NOW;
-        assertEquals("01/01/2017", startDate.format(FORMAT));
-        assertEquals("23/03/2018", endDate.format(FORMAT));
     }
 
     @Test
     public void currentAndLast11Months() {
         LocalDate startDate = NOW.minusMonths(11);
         LocalDate endDate = NOW;
-        assertEquals("15/04/2017", startDate.format(FORMAT));
-        assertEquals("23/03/2018", endDate.format(FORMAT));
     }
 
     @Test
     public void currentAndLast3Years() {
         LocalDate startDate = NOW.minusYears(3).withDayOfYear(1);
         LocalDate endDate = NOW;
-        assertEquals("01/01/2015", startDate.format(FORMAT));
-        assertEquals("23/03/2018", endDate.format(FORMAT));
     }
 
     @Test
     public void currentAndLast() {
         LocalDate startDate = NOW.minusMonths(1).withDayOfMonth(1);
         LocalDate endDate = NOW;
-        assertEquals("01/02/2018", startDate.format(FORMAT));
-        assertEquals("23/03/2018", endDate.format(FORMAT));
     }
 
     @Test
-    public void currentAndLAst15Days() {
+    public void currentAndLast15Days() {
         LocalDate startDate = NOW.minusDays(1L + 5);
         LocalDate endDate = NOW;
-        assertEquals("28/02/2018", startDate.format(FORMAT));
-        assertEquals("23/03/2018", endDate.format(FORMAT));
     }
 }
+/**/
